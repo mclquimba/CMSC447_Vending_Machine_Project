a4893b5e1a4ab806b856b8ff32fe6462152a2750
Ajibu Pascali <ajibupascali@MacBook-Pro-de-Ajibu.local>
2026-04-08 15:45:02 -0400

    routing all html pages

--- app.py
+++ app.py
@@ -1,3 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
-@app.route()
+
+@app.route("/")
+def welcome():
+    return render_template('welcome.html')
+
+@app.route("/dashboard.html")
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route("/stock.html")
+def stock():
+    return render_template('stock.html')
+@app.route("restock.html")
+def restock():
+    return render_template('restock.html')
+    
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+    
