bf7acb58e367b61652f56c4add815fb6a2bba2a6
Ajibu Pascali <ajibupascali@MacBook-Pro-de-Ajibu.local>
2026-04-13 15:42:20 -0400

    appending data between tables

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
 @app.route("/", methods = ['GET', 'POST'])
 def welcome():
     error = None
@@ -29,41 +30,38 @@ def dashboard():
 @app.route("/restock")
 def restock():
     return render_template('restock.html')
-    
-if __name__ == '__main__':
-    app.run(debug=True)
 
 inventory = [
 
-    {"slot": "A1", "item": "Tissues", "price": 1.00, "quantity": 2, "max": 6},
-    {"slot": "A2", "item": "Pens", "price": 1.50, "quantity": 5, "max": 5},
-    {"slot": "A3", "item": "Markers", "price": 1.55, "quantity": 1, "max": 5},
-    {"slot": "A4", "item": "Pencils", "price": 1.60, "quantity": 0, "max": 6},
+    {"slot": "A1", "item": "Tissues", "quantity": 2, "price": 1.00, "max": 6},
+    {"slot": "A2", "item": "Pens", "quantity": 5,"price": 1.50, "max": 5},
+    {"slot": "A3", "item": "Markers", "quantity": 1, "price": 1.55, "max": 5},
+    {"slot": "A4", "item": "Pencils", "quantity": 0, "price": 1.60, "max": 6},
 
-    {"slot": "B1", "item": "Notebooks", "price": 2.00, "quantity": 4, "max": 5},
-    {"slot": "B2", "item": "Sticky Notes", "price": 2.05, "quantity": 2, "max": 5},
-    {"slot": "B3", "item": "Highlighters", "price": 2.10, "quantity": 5, "max": 5},
-    {"slot": "B4", "item": "Erasers", "price": 2.15, "quantity": 1, "max": 5},
+    {"slot": "B1", "item": "Notebooks", "quantity": 4, "price": 2.00, "max": 5},
+    {"slot": "B2", "item": "Sticky Notes", "quantity": 2, "price": 2.05,"max": 5},
+    {"slot": "B3", "item": "Highlighters", "quantity": 5, "price": 2.10, "max": 5},
+    {"slot": "B4", "item": "Erasers", "quantity": 1, "price": 2.15, "max": 5},
 
-    {"slot": "C1", "item": "Spoons", "price": 0.75, "quantity": 3, "max": 6},
-    {"slot": "C2", "item": "Forks", "price": 0.80, "quantity": 6, "max": 6},
-    {"slot": "C3", "item": "Napkins", "price": 0.85, "quantity": 0, "max": 6},
-    {"slot": "C4", "item": "Wet Wipes", "price": 0.90, "quantity": 2, "max": 6},
+    {"slot": "C1", "item": "Spoons", "quantity": 3, "price": 1.75, "max": 6},
+    {"slot": "C2", "item": "Forks",  "quantity": 6, "price": 1.80, "max": 6},
+    {"slot": "C3", "item": "Napkins", "quantity": 0, "price": 1.85, "max": 6},
+    {"slot": "C4", "item": "Wet Wipes", "quantity": 2, "price": 1.90, "max": 6},
 
-    {"slot": "D1", "item": "Hand Sanitizer", "price": 2.50, "quantity": 3, "max": 5},
-    {"slot": "D2", "item": "Bandages", "price": 2.55, "quantity": 5, "max": 5},
-    {"slot": "D3", "item": "Pain Gel", "price": 2.60, "quantity": 1, "max": 5},
-    {"slot": "D4", "item": "Safety Pins", "price": 2.65, "quantity": 0, "max": 5},
+    {"slot": "D1", "item": "Hand Sanitizer", "quantity": 3, "price": 2.50,"max": 5},
+    {"slot": "D2", "item": "Bandages", "quantity": 5, "price": 2.55, "max": 5},
+    {"slot": "D3", "item": "Pain Gel",  "quantity": 1, "price": 2.60, "max": 5},
+    {"slot": "D4", "item": "Safety Pins", "quantity": 0, "price": 2.65, "max": 5},
 
-    {"slot": "E1", "item": "Advil", "price": 3.00, "quantity": 2, "max": 5},
-    {"slot": "E2", "item": "Aspirin", "price": 3.05, "quantity": 5, "max": 5},
-    {"slot": "E3", "item": "Cold Medicine", "price": 3.10, "quantity": 1, "max": 5},
-    {"slot": "E4", "item": "Tampons", "price": 3.15, "quantity": 0, "max": 5},
+    {"slot": "E1", "item": "Advil",  "quantity": 2,"price": 3.00, "max": 5},
+    {"slot": "E2", "item": "Aspirin", "quantity": 5, "price": 3.05, "max": 5},
+    {"slot": "E3", "item": "Cold Medicine", "quantity": 1, "price": 3.10, "max": 5},
+    {"slot": "E4", "item": "Tampons", "quantity": 0, "price": 3.15, "max": 5},
 
-    {"slot": "F1", "item": "Pads", "price": 3.20, "quantity": 3, "max": 5},
-    {"slot": "F2", "item": "Comb", "price": 3.25, "quantity": 5, "max": 5},
-    {"slot": "F3", "item": "Phone Charger", "price": 5.00, "quantity": 1, "max": 4},
-    {"slot": "F4", "item": "Earbuds", "price": 5.50, "quantity": 4, "max": 4}
+    {"slot": "F1", "item": "Pads", "quantity": 3, "price": 3.20, "max": 5},
+    {"slot": "F2", "item": "Comb", "quantity": 5, "price": 3.25, "max": 5},
+    {"slot": "F3", "item": "Phone Charger", "quantity": 1, "price": 5.00, "max": 4},
+    {"slot": "F4", "item": "Earbuds", "quantity": 4, "price": 5.50, "max": 4}
 
 ]
 
@@ -72,7 +70,7 @@ def get_status(item):
 
     if item["quantity"] == 0:
         return "OUT"
-    elif ratio < 0.8:
+    elif ratio < 0.5:
         return "LOW"
     else:
         return "GOOD"
@@ -82,6 +80,26 @@ def get_status(item):
 def stock():
     for item in inventory:
         item["status"] = get_status(item)
-    return render_template("stock.html", items = inventory) 
+    return render_template('stock.html', items = inventory) 
 
     
+@app.route("/buy/<slot>")
+def buy(slot):
+    for item in inventory:
+        if item["slot"] == slot:
+            if item["quantity"] > 0:
+                item["quantity"] -= 1
+                break
+    return redirect(url_for("stock"))
+            
+@app.route("/restock")
+def restock_item():
+    restockList = []
+    for item in inventory:
+        item["status"] = get_status(item)
+        if item["status"] in ["LOW","OUT"]
+        restockList.append(item)
+    return render_template("restock.html", items = restockList)
+
+if __name__ == '__main__':
+    app.run(debug=True)
