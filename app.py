580a7d49e6a351dd41b5669095224bb9bcc1b421
Ajibu Pascali <ajibupascali@MacBook-Pro-de-Ajibu.local>
2026-04-13 10:55:52 -0400

    updated app files

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def welcome():
-    return render_template('welcome.html')
+    error = None
 
-@app.route("/dashboard.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not username or not password:
+            error = "Please enter both username and password."
+        elif password != "umbc123":
+            error = "Invalid password."
+        else:
+            return redirect(url_for("dashboard"))
+    return render_template('welcome.html', error = error)
+
+@app.route("/dashboard")
 def dashboard():
-    return render_template('dashboard.html')
+    items_out = 6
+    low_items = 4
+    last_checked = "4/8/26 10:30PM"
+    staff_name = "UMBC3212"
+    
+    return render_template("dashboard.html", items_out = items_out, low_items = low_items, last_checked = last_checked, staff_name = staff_name)
 
-@app.route("/stock.html")
+@app.route("/stock")
 def stock():
     return render_template('stock.html')
-@app.route("restock.html")
+@app.route("/restock")
 def restock():
     return render_template('restock.html')
     
